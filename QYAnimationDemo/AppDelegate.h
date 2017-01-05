e5f5ce1f29b5d0a8e35192949ae94059d2c319a4
yixiong <yixiong@chemanman.com>
2017-01-05 10:58:30 +0800

    Initial Commit

---
+++ QYAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYAnimationDemo
+//
+//  Created by qianye on 17/1/5.
+//  Copyright © 2017年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYAnimationDemo
+//
+//  Created by qianye on 17/1/5.
+//  Copyright © 2017年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
